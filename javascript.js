33dbcdff7686e59fff3043490d6c4cbfee6b3460
jhovannny <al221911809@gmail.com>
2022-07-03 19:32:41 -0500

    first commit

---
+++ javascript.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $("#leer").click(function (e) { 
+        e.preventDefault();
+    $.get("archivo.txt", 
+        function (data, textStatus, jqXHR) {
+            console.log(data);
+       
+       console.log(textStatus);
+       console.log(jqXHR);
+       
+        }   );
+    });
+});
