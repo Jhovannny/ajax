b29422bad89cb46e8b67d331bebb7b12d33b1f17
jhovannny <al221911809@gmail.com>
2022-07-03 20:18:02 -0500

    txt a json

--- javascript.js
+++ javascript.js
@@ -10,6 +10,8 @@ $(document).ready(function () {
        
         }   );
     });
+
+});
 $('#empleados').click(function(e){
 e.preventDefault();
 $.get("empleados.json", function(data){
@@ -20,6 +22,10 @@ Puesto: ${data.puesto} <br>
 Edad: ${data.edad} <br>
 `
 );
+});
+});
+
+
 $('leerArray').click(function(e){
 e.preventDefault();
 $.get("empleados.json",function(data){
@@ -31,6 +37,61 @@ $.each(data, function (index, item) {
 });
 });
 });
+
+
+
+
+
+$('#leerGetSON').click(function (e) { 
+    e.preventDefault();
+    $.getJSON("empleados.txt",function(data){
+        data=JSON.parse(data);//covertir el string en json
+        console.log(data);
+    });
+    $.getJSON("empleados.json",function(data){
+        console.log(data);
+    });
 });
-});
+$(document).ready(function () {
+    $("#leer").click(function (e) { 
+        e.preventDefault();
+        $.get("archivo.txt",function(data,textStatus,jqXHR){
+            console.log(data);
+            //console.log(textStatus);
+            //console.log(jqXHR);
+        });
+    });
+    $('#leerEmpleado').click(function (e) { 
+        e.preventDefault();
+        $.get("empleados.json",function(data){
+            console.log(data);
+            $('#datosEmpleado').html(`
+            Nombre: ${data.nombre} <br>
+            Puesto: ${data.puesto} <br>
+            Edad: ${data.edad} <br>
+            `);
+        });
+    });
+    $('#leerArregloEmpleado').click(function (e) { 
+        e.preventDefault();
+        $('#listaEmpleados').html('');
+        $.get("empleados.json",function(data){
+            //console.log(data);
+            $.each(data, function (index, item) { 
+                 $('#listaEmpleados').html($('#listaEmpleados').html()+`
+                 <li> ${item.nombre} -- ${item.puesto} </li>
+                 `);
+            });
+        });
+    });
+    $('#leerGetSON').click(function (e) { 
+        e.preventDefault();
+        $.getJSON("empleados.txt",function(data){
+            data=JSON.parse(data);
+            console.log(data);
+        });
+        $.getJSON("empleados.json",function(data){
+            console.log(data);
+        });
+    });
 });
